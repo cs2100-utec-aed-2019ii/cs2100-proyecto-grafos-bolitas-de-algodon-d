054f22b83940a6a4450aad3513644462488e649d
Andre <andre.segovia@utec.edu.pe>
2019-10-16 16:31:14 -0500

    ESTE ES UN MENSAJE DE COMIT OWO

--- Vertex.hpp
+++ Vertex.hpp
@@ -1,6 +1,17 @@
 #ifndef VERTEX_H
 #define VERTEX_H
+#include "./ForwardList.hpp"
+#include "./Link.hpp"
 
-
+template<typename T>
+struct Vertex
+{
+  List<Vertex<T>*> nodes;
+  float x;
+  float y;
+  T dato;
+  List<Link<T>*> links;
+  Vertex(T _dato, float _x, float _y) : dato(_dato), x(_x), y(_y){}
+};
 
 #endif
