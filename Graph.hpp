2930a5e846b5752a8c78902b5e1fee56566e691d
osoman2 <osmancampeon69@hotmail.com>
2019-10-16 17:17:39 -0500

    definiciones

--- Graph.hpp
+++ Graph.hpp
@@ -3,15 +3,81 @@
 #include "./ForwardList.hpp"
 #include "./Vertex.hpp"
 #include "./Link.hpp"
+#include <string>
+
 
 template <typename T>
 class graph
 {
 private:
-  
+  List<Node<T>*> nodos;
+  List<Link<T>*> links ;
+  void processvtk(){
+
+    }
 public:
   graph (){}
+  graph (graph &grafo){
+
+  }
+  
+  graph(std::string nombre_archivo){
+
+  }
+
   virtual ~graph (){}
+
+  void insert_nodo(Node<T>* nodo ){
+
+  }
+
+  void make_link(Node<T>*nodo_1,Node<T>*nodo_2 ){
+
+  }
+  void rm_link(Node<T>*nodo_1,Node<T>*nodo_2){
+
+  }
+
+  void rm_node(Node<T>* nodo){
+
+  }
+  bool is_connect (){
+
+  }
+  bool is_bipartited(){
+
+  }
+  float calc_density(){
+
+  }
+  graph prim(Node<T>* inicial){
+
+  }
+  graph kruskal(){
+
+  }
+
+//busquedas---------------------------
+
+  Node<T> * BFS(){
+
+  }
+  Node<T> * DFS(){
+    
+  }
+
+  List <Node<T>*> get_neighbort(){
+
+  }
+  
+
+  //auxiliares
+
+    void save(){
+
+    }
+
+
 };
 
 #endif
