ac5db47bd90655549d09804dba2a082fbedb1e4a
Andre <andre.segovia@utec.edu.pe>
2019-10-24 05:40:01 -0500

    Limpiado main

--- main.cpp
+++ main.cpp
@@ -5,15 +5,15 @@ using namespace std;
 
 int main()
 {
-  graph<false,char>* grafo = new graph<false,char>;
-  bool isdirected;
-  string filename;
-  Parser<char> parser(isdirected, &filename);
-  parser.import(grafo);
-  parser.save(grafo);
-  delete grafo;
-  grafo = nullptr;
-  grafo = (graph<false, char>*)parser.load();
-  delete grafo;
+  //graph<false,char>* grafo = new graph<false,char>;
+  //bool isdirected;
+  //string filename;
+  //Parser<char> parser(isdirected, &filename);
+  //parser.import(grafo);
+  //parser.save(grafo);
+  //delete grafo;
+  //grafo = nullptr;
+  //grafo = (graph<false, char>*)parser.load();
+  //delete grafo;
   return 0;
 }
