cfb6cddf73fc5c8304b9a5035efb9816b8f0ef01
vid58 <apple@Apples-MacBook-Pro.local>
2019-12-04 16:39:57 -0500

    algo :V

--- Screen.hpp
+++ Screen.hpp
@@ -5,6 +5,7 @@
 #include "./Graph.hpp"
 #include "./file_management/Parser.hpp"
 #include "./Macros.hpp"
+int llave1,llave2;
 
 typedef struct{
   GLfloat verticeXYZ[3];
@@ -30,6 +31,7 @@ private:
   EstadoRaton raton;
   float x,y;
 public:
+
   Screen(int &argc, char **argv, float x, float y, void(*draw)(void), void(mouse)(int, int, int, int), void(keys)(unsigned char,int, int)):values(nullptr), values2(nullptr), isdirected(false), pantalla_x(x), pantalla_y(y)
   {
     parser = new Parser<T>(isdirected, &filename);
@@ -94,6 +96,12 @@ public:
     {{posicion(310,pantalla_x),posicion(75,pantalla_y),0},{192.0/250.0,57.0/250.0,43.0/250.0}},
     {{posicion(310,pantalla_x),posicion(25,pantalla_y),0},{192.0/250.0,57.0/250.0,43.0/250.0}}
   };
+  Vertice Movimiento[4] = {
+    {{posicion(360,pantalla_x),posicion(75,pantalla_y),0},{192.0/250.0,57.0/250.0,43.0/250.0}},
+    {{posicion(360,pantalla_x),posicion(25,pantalla_y),0},{192.0/250.0,57.0/250.0,43.0/250.0}},
+    {{posicion(410,pantalla_x),posicion(75,pantalla_y),0},{192.0/250.0,57.0/250.0,43.0/250.0}},
+    {{posicion(410,pantalla_x),posicion(25,pantalla_y),0},{192.0/250.0,57.0/250.0,43.0/250.0}}
+  };
 
   void IniciarGLUT(){
     glutInitDisplayMode(GLUT_RGB|GLUT_SINGLE);
@@ -106,7 +114,14 @@ public:
   {
     glutMainLoop();
   }
+  void drawtext(const char *text, int length, float x,float y){
 
+    glRasterPos2f(x,y);
+    for(int i=0;i<length;i++){
+        glutBitmapCharacter(GLUT_BITMAP_9_BY_15,(int)text[i]);
+    }
+
+  }
   void drawHandler(void)
   {
     graph<false,T> *val = (isdirected)? (graph<false,T> *)values2 : values;
@@ -134,6 +149,13 @@ public:
          glVertex3fv(Siguiente[i].verticeXYZ);
         }
     glEnd();
+    
+    glBegin(GL_QUADS);
+        for (int i=0; i<4; i++) {
+         glColor3fv(Movimiento[i].colorRGB);
+         glVertex3fv(Movimiento[i].verticeXYZ);
+        }
+    glEnd();
 
     if(val)
     {
@@ -158,8 +180,38 @@ public:
           }
       );
       glEnd();
-    }
+      
+      if(llave1 == 1 && llave2 == 1)  
+      {     
+        glColor3f(1.0,0.0,0.0);
+        glPointSize(10); 
+        glBegin(GL_POINTS); 
+        glVertex2f(raton.x,raton.y);  
+        llave2 = 0;
+        glEnd();
+      }
 
+    }
+    std::string text1;
+    text1 ="Save";
+    glColor3f(0,0,0);
+    drawtext(text1.data(),text1.size(),posicion(60,pantalla_x),posicion(55,pantalla_y));
+
+    std::string text2;
+    text2 ="Read";
+    glColor3f(0,0,0);
+    drawtext(text2.data(),text2.size(),posicion(160,pantalla_x),posicion(55,pantalla_y));
+
+    std::string text3;
+    text3 ="Import";
+    glColor3f(0,0,0);
+    drawtext(text3.data(),text3.size(),posicion(260,pantalla_x),posicion(55,pantalla_y));
+
+    std::string text4;
+    text4 ="Movi";
+    glColor3f(0,0,0);
+    drawtext(text4.data(),text4.size(),posicion(360,pantalla_x),posicion(55,pantalla_y));
+   
     glPopMatrix();
 
     glFlush();
@@ -169,89 +221,112 @@ public:
     graph<false,T> *val = (isdirected)? (graph<false,T> *)values2 : values;
     if (boton == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
     {
-        raton.x = ((mousex)*2)/pantalla_x;
-        //x = raton.x;
-        //std::cout<<mousex<< " ";
-        //std::cout<<raton.x<<std::endl;
-        //std::cout<<"______"<<std::endl;
-        
-        raton.y = ((pantalla_y-mousey)*2)/pantalla_y;
-        y = raton.y;
-        //std::cout<<pantalla_y-mousey<< " ";
-        //std::cout<<raton.y<<std::endl;
+      
+      raton.x = ((mousex)*2)/pantalla_x;
+      //x = raton.x;
+      //std::cout<<mousex<< " ";
+      //std::cout<<raton.x<<std::endl;
+      //std::cout<<"______"<<std::endl;
+      
+      raton.y = ((pantalla_y-mousey)*2)/pantalla_y;
+      y = raton.y;
+      //std::cout<<pantalla_y-mousey<< " ";
+      //std::cout<<raton.y<<std::endl;
+      
+      if(posicion(62.7808,pantalla_x) <= raton.x && raton.x <= posicion(600,pantalla_x)){
+        if(posicion(125.67336,pantalla_y) <= raton.y && raton.y <= posicion(459.84,pantalla_y)){
+          llave2 = 1;
+          //aqui va la funcion insert
 
-        if(val)
-        {
-          Vertex<T> *temp = nullptr;
-          val->nodos.for_each(
-            [this, temp](Vertex<T> *i) mutable -> void{
-              if((valx(i->x))-posicion(4,pantalla_x) <= raton.x && raton.x <= (valx(i->x))+posicion(4,pantalla_x)){
-                if((valy(i->y))-posicion(4,pantalla_y) <= raton.y && raton.y <= (valy(i->y))+posicion(4,pantalla_y)){
-                  temp=i;
-                  
-                }
+        }
+      }
+
+      if(val)
+      {
+        Vertex<T> *temp = nullptr;
+        val->nodos.for_each(
+          [this, temp](Vertex<T> *i) mutable -> void{
+            if((valx(i->x))-posicion(4,pantalla_x) <= raton.x && raton.x <= (valx(i->x))+posicion(4,pantalla_x))
+            {
+              if((valy(i->y))-posicion(4,pantalla_y) <= raton.y && raton.y <= (valy(i->y))+posicion(4,pantalla_y))
+              {
+                temp=i;
+                llave2 = 0;
               }
-               if(temp)
-                  {
-                    //ELIMINAR NODO uwu
-                    std::cout<<"eliminar "<<std::endl; 
-
-                    std::cout<<valx(i->x)<<" "<<valy(i->y)<<std::endl;        
-                    temp=nullptr;
-                  }
             }
+            if(temp)
+            {
+              //ELIMINAR NODO uwu
+              std::cout<<"insertar "<<std::endl; 
 
-          );
+              std::cout<<valx(i->x)<<" "<<valy(i->y)<<std::endl;        
+              temp=nullptr;
+            }
+          }
+        );
+      }
+
+      if(posicion(60,pantalla_x) <= raton.x && raton.x <= posicion(110,pantalla_x)){
+        if(posicion(25,pantalla_y) <= raton.y && raton.y <= posicion(75,pantalla_y)){
+          save();
+        }
+      }
+      if(posicion(160,pantalla_x) <= raton.x && raton.x <= posicion(210,pantalla_x)){
+        if(posicion(25,pantalla_y) <= raton.y && raton.y <= posicion(75,pantalla_y)){
+          read();
+        }
+      }
+      if(posicion(260,pantalla_x) <= raton.x && raton.x <= posicion(310,pantalla_x)){
+        if(posicion(25,pantalla_y) <= raton.y && raton.y <= posicion(75,pantalla_y)){
+          import();
+          llave1 = 1;
+        }
+      }  
+      //nodo final
+       if(posicion(360,pantalla_x) <= raton.x && raton.x <= posicion(410,pantalla_x)){
+        if(posicion(25,pantalla_y) <= raton.y && raton.y <= posicion(75,pantalla_y)){
           
-        }
 
-        if(posicion(60,pantalla_x) <= raton.x && raton.x <= posicion(110,pantalla_x)){
-            if(posicion(25,pantalla_y) <= raton.y && raton.y <= posicion(75,pantalla_y)){
-               save();
-            }
         }
-        if(posicion(160,pantalla_x) <= raton.x && raton.x <= posicion(210,pantalla_x)){
-            if(posicion(25,pantalla_y) <= raton.y && raton.y <= posicion(75,pantalla_y)){
-               read();
-            }
-        }
-        if(posicion(260,pantalla_x) <= raton.x && raton.x <= posicion(310,pantalla_x)){
-            if(posicion(25,pantalla_y) <= raton.y && raton.y <= posicion(75,pantalla_y)){
-               import();
-            }
-        }  
-    }else if(boton == GLUT_RIGHT_BUTTON && state == GLUT_DOWN)
+      }
+
+    }
+    else if(boton == GLUT_RIGHT_BUTTON && state == GLUT_DOWN)
     {
-       glClear(GL_COLOR_BUFFER_BIT);  
-         if(val)
-        {
-          Vertex<T> *temp = nullptr;
-          val->nodos.for_each(
-            [this, temp](Vertex<T> *i) mutable -> void{
-              if((valx(i->x))-posicion(4,pantalla_x) <= raton.x && raton.x <= (valx(i->x))+posicion(4,pantalla_x)){
-                if((valy(i->y))-posicion(4,pantalla_y) <= raton.y && raton.y <= (valy(i->y))+posicion(4,pantalla_y)){
-                  temp=i;
-                  
-                }
+      glClear(GL_COLOR_BUFFER_BIT);  
+      if(val)
+      {
+        Vertex<T> *temp = nullptr;
+        val->nodos.for_each(
+          [this, temp](Vertex<T> *i) mutable -> void{
+            if((valx(i->x))-posicion(4,pantalla_x) <= raton.x && raton.x <= (valx(i->x))+posicion(4,pantalla_x))
+            {
+              if((valy(i->y))-posicion(4,pantalla_y) <= raton.y && raton.y <= (valy(i->y))+posicion(4,pantalla_y))
+              {
+                temp=i;
               }
-               if(temp)
-                  {
-                    //ELIMINAR NODO uwu
-                  
-                    std::cout<<"insertar "<<std::endl;         
-
-                    std::cout<<valx(i->x)<<" "<<valy(i->y)<<std::endl;
-                    temp=nullptr;
-                  }
             }
+            if(temp)
+            {
+              //ELIMINAR NODO uwu
+            
+              std::cout<<"eliminar "<<std::endl;         
 
-          );
+              std::cout<<valx(i->x)<<" "<<valy(i->y)<<std::endl;
+              temp=nullptr;
+            }
+          }
+        );
+      }
+
+       //nodo final
+       if(posicion(360,pantalla_x) <= raton.x && raton.x <= posicion(410,pantalla_x)){
+        if(posicion(25,pantalla_y) <= raton.y && raton.y <= posicion(75,pantalla_y)){
           
         }
-                
+      }
     }
     glutPostRedisplay();  
-
   }
   void save()
   {
